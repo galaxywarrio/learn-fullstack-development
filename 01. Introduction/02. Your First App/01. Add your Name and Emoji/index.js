16a9103c87462a4c927f48e8d8104ce6e98c68cf
Pranav Patle <pranavpatle17129@gmail.com>
2026-02-08 14:15:38 +0530

    2.1 task done
    Update name and emoji in the App rendering

--- 01. Introduction/02. Your First App/01. Add your Name and Emoji/index.js
+++ 01. Introduction/02. Your First App/01. Add your Name and Emoji/index.js
@@ -79,4 +79,4 @@ const App = (props) => {
 };
 
 // CHALLENGE: add your own name and emoji to the website
-ReactDOM.render(<App name="Someone" emoji="👋"/>, document.getElementById("root"));
+ReactDOM.render(<App name="Billu" emoji="🙌"/>, document.getElementById("root"));
